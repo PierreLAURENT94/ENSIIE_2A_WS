4244d0b0ce55e145bd7b5308b59586a53e4875cf
Pierre LAURENT <pierrelaurent@numericable.fr>
2024-01-09 22:52:04 +0100

    trains sur 3 mois

--- rest/src/DataFixtures/TrainFixture.php
+++ rest/src/DataFixtures/TrainFixture.php
@@ -20,65 +20,70 @@ class TrainFixture extends Fixture
             foreach ($stations as $arrivalStation) {
                 if ($departureStation->getCity() != $arrivalStation->getCity()) {
                     switch ($_ENV['COMPANY']) {
+                        // Info : TGV INOUI => 29160 trains
                         case 'TGV INOUI':
                             $duration = DateInterval::createFromDateString(rand(30, 360) . " minutes");
+                            for ($dayAdd = 0; $dayAdd < 90; $dayAdd++) {
+                                $departureTrain1 = new DateTime();
+                                $departureTrain1->modify("+$dayAdd day");
+                                $departureTrain1->setTime(rand(8, 11), rand(0, 59));
+                                $arrivalTrain1 = clone $departureTrain1;
+                                $arrivalTrain1->add($duration);
+                                $priceFirstTrain1 = rand(5000, 15000);
+                                $train1 = new Train;
+                                $train1
+                                    ->setDepartureStation($departureStation)
+                                    ->setDepartureDateTime($departureTrain1)
+                                    ->setArrivalStation($arrivalStation)
+                                    ->setArrivalDateTime($arrivalTrain1)
+                                    ->setSeatsAvailableBusiness(rand(0, 25))
+                                    ->setPriceBusiness(rand(30000, 90000) / 100)
+                                    ->setSeatsAvailableFirst(rand(0, 150))
+                                    ->setPriceFirst($priceFirstTrain1 / 100)
+                                    ->setSeatsAvailableStandard(rand(0, 325))
+                                    ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain1)) / 100);
+                                $manager->persist($train1);
 
-                            $departureTrain1 = new DateTime();
-                            $departureTrain1->setTime(rand(8, 11), rand(0, 59));
-                            $arrivalTrain1 = clone $departureTrain1;
-                            $arrivalTrain1->add($duration);
-                            $priceFirstTrain1 = rand(5000, 15000);
-                            $train1 = new Train;
-                            $train1
-                                ->setDepartureStation($departureStation)
-                                ->setDepartureDateTime($departureTrain1)
-                                ->setArrivalStation($arrivalStation)
-                                ->setArrivalDateTime($arrivalTrain1)
-                                ->setSeatsAvailableBusiness(rand(0, 25))
-                                ->setPriceBusiness(rand(30000, 90000) / 100)
-                                ->setSeatsAvailableFirst(rand(0, 150))
-                                ->setPriceFirst($priceFirstTrain1 / 100)
-                                ->setSeatsAvailableStandard(rand(0, 325))
-                                ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain1)) / 100);
-                            $manager->persist($train1);
+                                $departureTrain2 = new DateTime();
+                                $departureTrain2->modify("+$dayAdd day");
+                                $departureTrain2->setTime(rand(12, 15), rand(0, 59));
+                                $arrivalTrain2 = clone $departureTrain2;
+                                $arrivalTrain2->add($duration);
+                                $priceFirstTrain2 = rand(5000, 15000);
+                                $train2 = new Train;
+                                $train2
+                                    ->setDepartureStation($departureStation)
+                                    ->setDepartureDateTime($departureTrain2)
+                                    ->setArrivalStation($arrivalStation)
+                                    ->setArrivalDateTime($arrivalTrain2)
+                                    ->setSeatsAvailableBusiness(rand(0, 25))
+                                    ->setPriceBusiness(rand(30000, 90000) / 100)
+                                    ->setSeatsAvailableFirst(rand(0, 150))
+                                    ->setPriceFirst($priceFirstTrain2 / 100)
+                                    ->setSeatsAvailableStandard(rand(0, 325))
+                                    ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain2)) / 100);
+                                $manager->persist($train2);
 
-                            $departureTrain2 = new DateTime();
-                            $departureTrain2->setTime(rand(12, 15), rand(0, 59));
-                            $arrivalTrain2 = clone $departureTrain2;
-                            $arrivalTrain2->add($duration);
-                            $priceFirstTrain2 = rand(5000, 15000);
-                            $train2 = new Train;
-                            $train2
-                                ->setDepartureStation($departureStation)
-                                ->setDepartureDateTime($departureTrain2)
-                                ->setArrivalStation($arrivalStation)
-                                ->setArrivalDateTime($arrivalTrain2)
-                                ->setSeatsAvailableBusiness(rand(0, 25))
-                                ->setPriceBusiness(rand(30000, 90000) / 100)
-                                ->setSeatsAvailableFirst(rand(0, 150))
-                                ->setPriceFirst($priceFirstTrain2 / 100)
-                                ->setSeatsAvailableStandard(rand(0, 325))
-                                ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain2)) / 100);
-                            $manager->persist($train2);
-
-                            $departureTrain3 = new DateTime();
-                            $departureTrain3->setTime(rand(15, 18), rand(0, 59));
-                            $arrivalTrain3 = clone $departureTrain3;
-                            $arrivalTrain3->add($duration);
-                            $priceFirstTrain3 = rand(5000, 15000);
-                            $train3 = new Train;
-                            $train3
-                                ->setDepartureStation($departureStation)
-                                ->setDepartureDateTime($departureTrain3)
-                                ->setArrivalStation($arrivalStation)
-                                ->setArrivalDateTime($arrivalTrain3)
-                                ->setSeatsAvailableBusiness(rand(0, 25))
-                                ->setPriceBusiness(rand(30000, 90000) / 100)
-                                ->setSeatsAvailableFirst(rand(0, 150))
-                                ->setPriceFirst($priceFirstTrain3 / 100)
-                                ->setSeatsAvailableStandard(rand(0, 325))
-                                ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain3)) / 100);
-                            $manager->persist($train3);
+                                $departureTrain3 = new DateTime();
+                                $departureTrain3->modify("+$dayAdd day");
+                                $departureTrain3->setTime(rand(15, 18), rand(0, 59));
+                                $arrivalTrain3 = clone $departureTrain3;
+                                $arrivalTrain3->add($duration);
+                                $priceFirstTrain3 = rand(5000, 15000);
+                                $train3 = new Train;
+                                $train3
+                                    ->setDepartureStation($departureStation)
+                                    ->setDepartureDateTime($departureTrain3)
+                                    ->setArrivalStation($arrivalStation)
+                                    ->setArrivalDateTime($arrivalTrain3)
+                                    ->setSeatsAvailableBusiness(rand(0, 25))
+                                    ->setPriceBusiness(rand(30000, 90000) / 100)
+                                    ->setSeatsAvailableFirst(rand(0, 150))
+                                    ->setPriceFirst($priceFirstTrain3 / 100)
+                                    ->setSeatsAvailableStandard(rand(0, 325))
+                                    ->setPriceStandard(rand(3500, min(10500, $priceFirstTrain3)) / 100);
+                                $manager->persist($train3);
+                            }
                             break;
                     }
                 }
